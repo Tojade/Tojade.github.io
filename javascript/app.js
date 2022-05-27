e8c2e100337654e343c4ceb4e7e375599ade8e69
Jaiden-2 <106224483+Tojade@users.noreply.github.com>
2022-05-27 21:18:54 +1000

    Added new files

---
+++ javascript/app.js
@@ -0,0 +1,80 @@
+// Scroll function with nav bar
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20)
+            $(".navbar").addClass("sticky");
+        else
+            $(".navbar").removeClass("sticky");
+    });
+
+    $('.menu-toggler').click(function () {
+        $(this).toggleClass("active");
+        $(".navbar-menu").toggleClass("active");
+    });
+
+    $(".works").magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: { enabled: true }
+    });
+
+    // Animation for heading
+    var TxtType = function (el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function () {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+
+        setTimeout(function () {
+            that.tick();
+        }, delta);
+    };
+    // Type write function
+    window.onload = function () {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i = 0; i < elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // Type writer css injection
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff; color: #fff}"
+        document.body.appendChild(css);
+    };
+});
+
+
